d4183c676e17c97a47a9feb9188867a5edeb4907
sEi <manvisdk@gmail.com>
2023-06-30 16:45:56 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # SimpleHtmlChatAgents
 Very simple chat interface with short time memory. List with different agents
+
+This is a simple chat application that uses OpenAI's GPT-3.5-Turbo model to generate responses. The application allows you to choose from different agents, each with their own unique personality and style of communication.
+
+## Features
+
+- Multiple agents to choose from, each with a unique personality.
+- Option to select different models based on token count.
+- Displays the token count used for each response.
+- Conversation history is maintained for the duration of the session.
+
+## How to Use
+
+1. Select a model from the dropdown menu. The options are GPT-3.5-Turbo (4096 Tokens), GPT-3.5-Turbo-16K (16384 Tokens), and GPT-4 (8192 Tokens).
+2. Select an agent from the dropdown menu. The options are Default, Marvin (the Paranoid Android from The Hitchhiker's Guide to the Galaxy), and The Pirate (Dorothy the pirate).
+3. Type your message in the input field and click the "Send" button or press Enter to send it.
+4. The assistant's response will appear in the chat log, along with the token count used for the response.
+5. If you want to change the agent, you can do so from the dropdown menu. Note that changing the agent will reset the chat.
+
+## Code Structure
+
+- `agents.js`: This file contains the definitions for the different agents.
+- `functions.js`: This file contains the main functionality of the application, including sending messages and handling responses.
+- `index.html`: This is the main HTML file for the application.
+- `styles.css`: This file contains the CSS styles for the application.
+
+## Note
+
+This application uses OpenAI's GPT-3.5-Turbo model, which is a powerful language model capable of understanding and generating human-like text. However, it's important to note that the model doesn't have any real understanding of the world and doesn't have access to personal data about individuals unless it has been shared in the course of the conversation. It generates responses based on a mixture of licensed data, data created by human trainers, and publicly available data.
+
+## Disclaimer
+
+This application is for demonstration purposes only and is not intended to be used for any harmful or malicious activities. The developers are not responsible for any misuse of the application.
---
+++ html/functions.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+    const API_ENDPOINT = 'https://api.openai.com/v1/chat/completions';
+
+    // Initialize an empty array to store the conversation history
+    let conversationHistory = [];
+
+    // Populate the agent dropdown
+    function populateAgentDropdown() {
+        let dropdown = $('#agent-dropdown');
+        dropdown.empty();
+        //dropdown.append('<option value="">Select an agent</option>');
+
+        $.each(agents.agents, function (key, agent) {
+            dropdown.append('<option value="' + key + '">' + agent.title + '</option>');
+        });
+        showAgentInfo()
+    }
+
+    // Get the selected agent as an object
+    function getSelectedAgent() {
+        let selectedAgentKey = $('#agent-dropdown').val();
+        return agents.agents[selectedAgentKey];
+    }
+
+    function sendMessage() {
+        const userInput = $('#user-input').val();
+        if (userInput.trim() === '') {
+            return;
+        }
+
+        $('#chat-log').append('<p><strong>You:</strong> ' + userInput + '</p>');
+        $('#user-input').val('');
+
+        // Add the user's message to the conversation history
+        conversationHistory.push({ role: 'user', content: userInput });
+
+        let sysmes = getSelectedAgent().systemprompt;
+
+        let selectedModel = $('#model-dropdown').val();
+
+        $.ajax({
+            url: API_ENDPOINT,
+            type: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer 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'
+            },
+            data: JSON.stringify({
+                model: selectedModel,
+                // Prepend the conversation history to the messages
+                messages: [{ role: 'system', content: sysmes }].concat(conversationHistory)
+            }),
+            success: function (response) {
+                const answer = response.choices[0].message.content;
+                $('#chat-log').append('<p><strong>Assistant:</strong> ' + answer + '</p>');
+
+                // Add the assistant's message to the conversation history
+                conversationHistory.push({ role: 'assistant', content: answer });
+
+                // Update the token count display
+                const tokensUsed = response.usage.total_tokens;
+                $('#TokenUse').text('Tokens Used: ' + tokensUsed);
+            },
+            error: function (xhr) {
+                console.error(xhr.responseText);
+            }
+        });
+    }
+
+    // Initialize the agent dropdown
+    populateAgentDropdown();
+
+    $('#send-btn').click(sendMessage);
+    $('#user-input').keypress(function (e) {
+        if (e.which === 13) {
+            sendMessage();
+        }
+    });
+
+    // Get the selected agent when the dropdown is changed
+    $('#agent-dropdown').change(function () {
+        showAgentInfo()
+        // Clear the conversation history
+        conversationHistory = [];
+        // Clear the chat log
+        $('#chat-log').empty();
+        $('#TokenUse').text('Tokens Used: 0');
+    });
+
+    function showAgentInfo() {
+        let selectedAgent = getSelectedAgent();
+        $('#agent-info').html(selectedAgent.info);
+    }
+});
