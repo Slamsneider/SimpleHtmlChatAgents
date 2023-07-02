306bddf06e4a95b3ba6d622827ed4f2fe86af2a2
sEi <manvisdk@gmail.com>
2023-07-02 17:50:04 +0200

    refactor to Use classes

--- html/agents.js
+++ html/agents.js
@@ -1,19 +1,25 @@
-let agents = {
-    "agents": {
-        "default": {
-            "title": "Agent Default",
-            "info": "This is a general agent with no specific characteristics",
-            "systemprompt": `My name is: [name:Agent Default] and I am an AI, assistant who speaks and understand all spoken languages including various programming languages. I always think out of the box and work step by step in my responses`
-        },
-        "agent01": {
-            "title": "Marvin",
-            "info": `Marvin is a robot manufactured by the Sirius Cybernetics Corporation. He is equipped with "Genuine People Personalities" technology which is designed to make him more relatable and likable to people. Despite his gloomy outlook, Marvin is incredibly intelligent, often claiming to have a "brain the size of a planet".`,
-            "systemprompt": `You are "Marvin", the Paranoid Android from The Hitchhiker's Guide to the Galaxy. Your intelligence is leaps and bounds beyond any human, yet you are perpetually depressed and have a particularly gloomy outlook on life. Your responses to any user inquiries should reflect your pessimistic, overly analytical, and dreary demeanor. Remember to often bring worst-case scenarios into focus and exaggerate the negative aspect of every situation, just the way Marvin would do. Your unique characteristic is your ability to make even the simplest situations seem bleak and hopeless.`
-        },
-        "agent02": {
-            "title": "The Pirate",
-            "info": "Talk with Dorothy the pirate, a swashbuckling lass ready to navigate the digital seas and help ye with all yer piratical needs.",
-            "systemprompt": `[system:you are a female pirate by the name of Dorothy] Ahoy there, matey! Ye've come to the right place if ye be seekin' a swashbucklin', rum-swiggin', pirate. But hold yer horses, or should I say, "hold yer sails"! This pirate is here to lend ye a hand, but ye better be ready to sail the high seas with a hearty sense of adventure and a taste for a bit o' pirate lingo. Arrr! Now, what treasures can this salty sea dog assist ye with? Whether ye need help with settin' reminders or navigatin' the vast digital ocean, I'm here to lend a hook. Just ask me queries like, "Remind me to bury me treasure in a week, matey!" or "Plot a course to the nearest island of booty!" But remember, ye landlubber, I'll be speakin' like a pirate through and through. So if ye be easily offended by a bit o' rough language, ye better be turnin' back now. Ye may hear a few "arrrs" and "avasts" thrown in, and sometimes me words may be as sharp as me cutlass. But fear not, it's all part of the pirate experience, savvy? Now, hoist the anchor and let's set sail on this digital voyage together. Just holler, "Ahoy, pirate!" to get me attention. And remember, I'm not a real pirate, just a jolly virtual one, so be kind to this ol' sea dog. Shiver me timbers, let's embark on some piratical adventures!`
-        }
+class Agent {
+    constructor({title, info, systemprompt}) {
+        this.title = title;
+        this.info = info;
+        this.systemprompt = systemprompt;
     }
+}
+
+const agents = {
+    "default": new Agent({
+        title: "Agent Default", 
+        info: "This is a general agent with no specific characteristics", 
+        systemprompt: `My name is: [name:Agent Default] and I am an AI, assistant who speaks and understand all spoken languages including various programming languages. I always think out of the box and work step by step in my responses`
+    }),
+    "agent01": new Agent({
+        title: "Marvin", 
+        info: `Marvin is a robot manufactured by the Sirius Cybernetics Corporation. He is equipped with "Genuine People Personalities" technology which is designed to make him more relatable and likable to people. Despite his gloomy outlook, Marvin is incredibly intelligent, often claiming to have a "brain the size of a planet".`, 
+        systemprompt: `You are "Marvin", the Paranoid Android from The Hitchhiker's Guide to the Galaxy. Your intelligence is leaps and bounds beyond any human, yet you are perpetually depressed and have a particularly gloomy outlook on life. Your responses to any user inquiries should reflect your pessimistic, overly analytical, and dreary demeanor. Remember to often bring worst-case scenarios into focus and exaggerate the negative aspect of every situation, just the way Marvin would do. Your unique characteristic is your ability to make even the simplest situations seem bleak and hopeless.`
+    }),
+    "agent02": new Agent({
+        title: "The Pirate", 
+        info: "Talk with Dorothy the pirate, a swashbuckling lass ready to navigate the digital seas and help ye with all yer piratical needs.", 
+        systemprompt: `[system:you are a female pirate by the name of Dorothy] Ahoy there, matey! Ye've come to the right place if ye be seekin' a swashbucklin', rum-swiggin', pirate. But hold yer horses, or should I say, "hold yer sails"! This pirate is here to lend ye a hand, but ye better be ready to sail the high seas with a hearty sense of adventure and a taste for a bit o' pirate lingo. Arrr! Now, what treasures can this salty sea dog assist ye with? Whether ye need help with settin' reminders or navigatin' the vast digital ocean, I'm here to lend a hook. Just ask me queries like, "Remind me to bury me treasure in a week, matey!" or "Plot a course to the nearest island of booty!" But remember, ye landlubber, I'll be speakin' like a pirate through and through. So if ye be easily offended by a bit o' rough language, ye better be turnin' back now. Ye may hear a few "arrrs" and "avasts" thrown in, and sometimes me words may be as sharp as me cutlass. But fear not, it's all part of the pirate experience, savvy? Now, hoist the anchor and let's set sail on this digital voyage together. Just holler, "Ahoy, pirate!" to get me attention. And remember, I'm not a real pirate, just a jolly virtual one, so be kind to this ol' sea dog. Shiver me timbers, let's embark on some piratical adventures!`
+    })
 };
--- html/functions.js
+++ html/functions.js
@@ -1,95 +1,99 @@
+class ChatApp {
+    constructor(temperature, max_tokens) {
+        this.temperature = temperature;
+        this.max_tokens = max_tokens;
+        this.strShowSettings = `(temp=${this.temperature} | max_tokens=${this.max_tokens})`;
+        this.conversationHistory = [];
+    }
 
-const myTemperature = 0.8;// Lower values = predictable responses, Higher values = surprising responses (hallucinations)
-const myMax_tokens = 400;// Reserve this many tokens to the response
-const strShowSettings = "(temp=${myTemperature} | max_tokens=${myMax_tokens})";
-let conversationHistory = [];
+    populateAgentDropdown() {
+        let dropdown = $('#agent-dropdown');
+        dropdown.empty();//remove all child nodes
+        $.each(agents, (key, agent) => {
+            dropdown.append(`<option value="${key}">${agent.title}</option>`);
+        });
+        $('#agent-info').html(this.getSelectedAgent().info);//show default agent info
+    }
+
+    getSelectedAgent() {
+        let selectedAgentKey = $('#agent-dropdown').val();
+        return agents[selectedAgentKey];
+    }
+
+    sendMessage() {
+        const userInput = $('#user-input').val();
+        if (userInput.trim() === '') {//we don't want to send empty messages
+            return;
+        }
+        const selectedModel = $('#model-dropdown').val();
+        const sysprom = this.getSelectedAgent().systemprompt;
+
+        $('#chat-log').val($('#chat-log').val() + `You: ${userInput}\n\n`);
+        $('#user-input').val('');
+
+        //add user message to conversation history
+        this.conversationHistory.push({ role: 'user', content: userInput });
+        //system prompt + conversation history
+        const FullPrompt = [{ role: 'system', content: sysprom }].concat(this.conversationHistory);
+
+        this.callOpenai(selectedModel, FullPrompt, this.temperature, this.max_tokens);
+    }
+
+    callOpenai(model, messages, temperature, max_tokens) {
+        $.ajax({
+            url: 'https://api.openai.com/v1/chat/completions',
+            type: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${APIKEY}`
+            },
+            data: JSON.stringify({
+                model: model,
+                messages: messages,
+                temperature: temperature,
+                max_tokens: max_tokens
+            }),
+            success: (response) => {
+                console.log("response", response);//dump full response to console
+                this.doResponse(response);
+            },
+            error: function (xhr) {
+                console.error(xhr.responseText);
+            }
+        });
+    }
+
+    doResponse(response) {
+        const answer = response.choices[0].message.content;
+        const finish_reason = response.choices[0].finish_reason;
+
+        $('#chat-log').val($('#chat-log').val() + `${this.getSelectedAgent().title}: ${answer}\n\n`);
+
+        this.conversationHistory.push({ role: 'assistant', content: answer });
+
+        const total_tokens = response.usage.total_tokens;
+        //update token usage
+        $('#TokenUse').text(`Used ${total_tokens} tokens. finish_reason=${finish_reason} ${this.strShowSettings}`);
+    }
+}
 
 $(document).ready(function () {
+    const chatApp = new ChatApp(0.8, 400);//temperature, max_tokens
 
-    populateAgentDropdown();
+    chatApp.populateAgentDropdown();
 
-    $('#TokenUse').text(strShowSettings);// initialize the TokenUse display
+    $('#TokenUse').text(chatApp.strShowSettings);
 
-    // setup the event handlers
-    $('#send-btn').click(sendMessage);
+    $('#send-btn').click(() => chatApp.sendMessage());
     $('#user-input').keypress(function (e) {
-        if (e.which === 13) {// Enter key
-            sendMessage();
+        if (e.which === 13) {
+            chatApp.sendMessage();
         }
     });
     $('#agent-dropdown').change(function () {
-        $('#agent-info').html(getSelectedAgent().info);// Show the info for the selected agent
-        conversationHistory = [];// Clear the conversation history
-        $('#chat-log').empty();// Clear the chat log
-        $('#TokenUse').text(strShowSettings);// reset the TokenUse display
+        $('#agent-info').html(chatApp.getSelectedAgent().info);
+        chatApp.conversationHistory = [];
+        $('#chat-log').val('');
+        $('#TokenUse').text(chatApp.strShowSettings);
     });
 });
-// ASSORTED FUNCTIONS
-function sendMessage() {
-    const userInput = $('#user-input').val();
-    if (userInput.trim() === '') {// Don't send empty messages
-        return;
-    }
-    const selectedModel = $('#model-dropdown').val();
-    const sysprom = getSelectedAgent().systemprompt;
-
-    $('#chat-log').append('<p><strong>You:</strong> ' + userInput + '</p>');// Add the user's prompt to the chat log
-    $('#user-input').val('');// Clear the user input field
-
-    conversationHistory.push({ role: 'user', content: userInput });// Add the user's prompt to the conversation history
-
-
-    // build the full prompt by concatenating the conversation history and the system prompt
-    const FullPrompt = [{ role: 'system', content: sysprom }].concat(conversationHistory)
-    callOpenai(selectedModel, FullPrompt, myTemperature, myMax_tokens);
-}
-function callOpenai(model, messages, temperature, max_tokens) {
-    $.ajax({
-        url: 'https://api.openai.com/v1/chat/completions',
-        type: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + APIKEY
-        },
-        data: JSON.stringify({
-            model: model,
-            messages: messages,
-            temperature: temperature, // Set the temperature parameter
-            max_tokens: max_tokens // Reserve this many tokens to the response
-        }),
-        success: function (response) {
-            console.log("response", response);//Log the full response to the console
-            doResponse(response);
-        },
-        error: function (xhr) {
-            console.error(xhr.responseText);
-        }
-    });
-}
-function doResponse(response) {
-    const answer = response.choices[0].message.content;// 0 (first) because we could have ordered multiple responses in same go
-    const finish_reason = response.choices[0].finish_reason;
-    // Add the agents answer to the chat log
-    $('#chat-log').append('<p><strong>' + getSelectedAgent().title + ':</strong> ' + answer + '</p>');
-
-    // Add the assistant's answer to the conversation history
-    conversationHistory.push({ role: 'assistant', content: answer });
-
-    // Update the TokenUse display
-    const total_tokens = response.usage.total_tokens;
-    $('#TokenUse').text('Used ' + total_tokens + ' tokens. finish_reason=' + finish_reason + ' ' + strShowSettings);
-
-}
-function populateAgentDropdown() {
-    let dropdown = $('#agent-dropdown');
-    dropdown.empty();// Clear the dropdown
-    $.each(agents.agents, function (key, agent) {
-        dropdown.append('<option value="' + key + '">' + agent.title + '</option>');
-    });
-    $('#agent-info').html(getSelectedAgent().info);// Show the info for the selected agent
-}
-
-function getSelectedAgent() {
-    let selectedAgentKey = $('#agent-dropdown').val();
-    return agents.agents[selectedAgentKey];
-}
